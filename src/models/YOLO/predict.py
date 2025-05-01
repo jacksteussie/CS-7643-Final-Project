b34a274001b37bd1952dd541f9a49838d7bbeac9
ochurchill1 <42160937+ochurchill1@users.noreply.github.com>
2025-04-30 22:48:47 -0400

    Feature/yolo code (#14)
    * Yolo scripts
    
    * Yolo readme and new infer file
    
    ---------
    
    Co-authored-by: Owen Churchill <ochurchill3@gatech.edu>

--- src/models/YOLO/predict.py
+++ src/models/YOLO/predict.py
@@ -1,4 +1,4 @@
 from ultralytics import YOLO
 
 model = YOLO('experiments/yolov8m_50epochs/weights/best.pt')
-model.predict(source='data/dota-mod/images/test/P1796__1024__3584___2688.jpg', save=True, conf=0.25)
+model.predict(source='data/dota-mod/images/test/P1796__640__3584___3072.jpg', save=True, conf=0.25)
